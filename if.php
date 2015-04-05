71460e57aebc4bc0b8f3ec56615dda955444fe08
MarioDener <MarioJose1693@gmail.com>
2015-04-04 19:35:39 -0600

    Sentencia If

---
+++ if.php
@@ -0,0 +1,22 @@
+<?php 
+	/*
+		Ejemplo de sentencia if
+	*/
+
+		$string = "codemario";
+		if ($string = "codemario") {
+			echo "Wooo ellos son increibles !.<br>";
+		}
+
+	/*
+		Ejemplo de if anidado
+	*/
+		$sstring = "mariodeber";
+		if ($string = "codemario") {
+			echo "Wooo ellos son increibles !.<br>";
+			if ($sstring = "mariodener") {
+				echo "Si MarioDener es parte de ellos<br>";
+			}
+		}
+
+ ?>
